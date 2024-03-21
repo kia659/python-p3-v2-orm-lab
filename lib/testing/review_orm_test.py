5d4220c1bbfd55689039076540a46a2541ef237f
Kia Neitzell <kia.neitzell@gmail.com>
2024-03-21 12:11:13 -0700

    ORM

--- lib/testing/review_orm_test.py
+++ lib/testing/review_orm_test.py
@@ -254,5 +254,5 @@ class TestReview:
                 (review1.id, review1.year, review1.summary, review1.employee_id))
         assert ((reviews[1].id, reviews[1].year, reviews[1].summary, reviews[1].employee_id) ==
                 (review2.id, review2.year, review2.summary, review2.employee_id))
-
- 
+        
+        
